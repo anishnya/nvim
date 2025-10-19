a9453a259cee2d066aa2b5709b6929da8ac08f7d
Anish Nyayachavadi <anishnya@umich.edu>
2025-10-19 04:16:01 -0700

    Simple addition of AI technology

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,6 +7,29 @@ vim.pack.add({
         src = "https://github.com/Saghen/blink.cmp",
         name = "blink",
     },
+    {
+        src = "https://github.com/zbirenbaum/copilot.lua",
+    },
+    {
+        src = "https://github.com/giuxtaposition/blink-cmp-copilot",
+        name = "blink-cmp-copilot",
+    },
+}, {
+    load = true,
 })
 
-require("blink.cmp").setup()
+require("copilot").setup()
+
+require("blink.cmp").setup({
+    sources = {
+        default = { "lsp", "path", "snippets", "buffer", "copilot" },
+        providers = {
+            copilot = {
+                name = "copilot",
+                module = "blink-cmp-copilot",
+                score_offset = 100,
+                async = true,
+            },
+        },
+    },
+})
